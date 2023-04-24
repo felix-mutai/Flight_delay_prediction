f201b5e20f9ea9a5320e5feb94a2415d73c47aac
Mutai <78373217+felix-mutai@users.noreply.github.com>
2023-04-24 18:50:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,106 @@ def main():
     # getting the input data from the user
 
     Day = st.text_input('Date of the Month')
-    Month = st.text_input('The month')
+    
+    Month = st.selectbox("Select the Month", ("January", "February", "March", "April", "May", "June", "July",
+                                              "August", "September", "October", "November", "December"))
+    if Month == "January":
+    Month = 1
+    elif Month == "February":
+    Month = 2
+    elif Month == "March":
+    Month = 3
+    elif Month == "April":
+    Month = 4
+    elif Month == "May":
+    Month = 5
+    elif Month == "June":
+    Month = 6
+    elif Month == "July":
+    Month = 7
+    elif Month == "August":
+    Month = 8
+    elif Month == "September":
+    Month = 9
+    elif Month == "October":
+    Month = 10
+    elif Month == "November":
+    Month = 11    
+    else:
+    Month = 12
     Airline = st.text_input('Flight airline')
-
+    
+    Airline = st.selectbox("Flight airline", ('05U', '3J', '3W', '5H', 'AT1', 'AT', 'B5', 'BA', 'CZ', 'D3', 'DO',
+       'EK', 'EY', 'F8', 'G9', 'KL', 'LH', 'LX', 'MK', 'MS', 'PW', 'QR',
+       'SA', 'SV', 'TK', 'TM', 'WB', 'WY', 'XU', '03R', 'ET', 'JM', 'KQ',
+       'UNS'))
+    if Airline == "05U":
+    Airline = 1
+    elif Airline == "3J":
+    Airline = 2
+    elif Airline == "3W":
+    Airline = 3
+    elif Airline == "5H":
+    Airline = 4
+    elif Airline == "AT1":
+    Airline = 5
+    elif Airline == "AT":
+    Airline = 6
+    elif Airline == "B5":
+    Airline = 7
+    elif Airline == "BA":
+    Airline = 8
+    elif Airline == "CZ":
+    Airline = 9
+    elif Airline == "D3":
+    Airline = 10
+    elif Airline == "DO":
+    Airline = 11
+    elif Airline == "EK":
+    Airline = 12
+    elif Airline == "EY":
+    Airline = 13
+    elif Airline == "F8":
+    Airline = 14
+    elif Airline == "G9":
+    Airline = 15
+    elif Airline == "KL":
+    Airline = 16
+    elif Airline == "LH":
+    Airline = 17
+    elif Airline == "LX":
+    Airline = 18
+    elif Airline == "MK":
+    Airline = 19
+    elif Airline == "MS":
+    Airline = 20
+    elif Airline == "PW":
+    Airline = 21
+    elif Airline == "QR":
+    Airline = 22
+    elif Airline == "SA":
+    Airline = 23
+    elif Airline == "SV":
+    Airline = 24
+    elif Airline == "TK":
+    Airline = 25
+    elif Airline == "TM":
+    Airline = 26
+    elif Airline == "WB":
+    Airline = 27
+    elif Airline == "WY":
+    Airline = 28
+    elif Airline == "XU":
+    Airline = 29
+    elif Airline == "03R":
+    Airline = 30
+    elif Airline == "ET":
+    Airline = 31
+    elif Airline == "JM":
+    Airline = 32
+    else:
+    Airline = 33
+   
     flightclass = st.selectbox("Flight Class (International/Domestic)", ("INT", "DOM"))
     if flightclass == "INT":
         flightclass = 1
